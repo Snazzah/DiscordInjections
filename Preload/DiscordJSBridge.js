f8a5f4064e156b785164ecfc6d4ac92f7d3936d6
Snazzah <suggesttosnazzy@gmail.com>
2017-10-24 16:36:56 -0500

    Add selectedUpdate again

--- Preload/DiscordJSBridge.js
+++ Preload/DiscordJSBridge.js
@@ -8,4 +8,22 @@ const Discord = require('discord.js');
 
 require('./DiscordMutator');
 
-module.exports = Discord.Client;
+class BridgedClient extends Discord.Client {
+    constructor(options) {
+        super(options);
+        let lastpath = window.location.pathname;
+        this.setInterval(() => {
+            if (lastpath === window.location.pathname) return;
+            this.emit('selectedUpdate', {
+                guild: this.guilds.get(lastpath.split('/')[2]),
+                channel: lastpath.split('/')[3] ? this.channels.get(lastpath.split('/')[3]) : undefined
+            }, {
+                guild: this.guilds.get(window.location.pathname.split('/')[2]),
+                channel: window.location.pathname.split('/')[3] ? this.channels.get(window.location.pathname.split('/')[3]) : undefined
+            });
+            lastpath = window.location.pathname;
+        }, window.DI_DATA.options.selectedUpdate);
+    }
+}
+
+module.exports = BridgedClient;
